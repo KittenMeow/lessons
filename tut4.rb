ea37cbdaa1e07c80bb061437403c7afbf20c0f94
Cat <39655575+KittenMeow@users.noreply.github.com>
2020-04-24 09:37:49 -0400

    Create tut4.rb

---
+++ tut4.rb
@@ -0,0 +1,9 @@
+print "Enter your name: "
+name = gets
+
+print "Enter two numbers: "
+num1 = gets.to_i
+num2 = gets.to_i
+
+puts num1.to_s + " + " + num2.to_s + " = " + (num1+num2).to_s
+puts "Your name is " + name
